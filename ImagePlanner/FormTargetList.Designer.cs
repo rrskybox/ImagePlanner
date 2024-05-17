6cbefe8497f9e2c27e4ac9dcfc73b9db4bce231c
Richard McAlister <rick-mcalister@msn.com>
2024-05-17 16:49:20 -0700

    Widened Target List form to accomodate larger names, etc without horizontal scrolling.

--- ImagePlanner/FormTargetList.Designer.cs
+++ ImagePlanner/FormTargetList.Designer.cs
@@ -30,9 +30,9 @@ namespace ImagePlanner
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.TargetDataGrid = new System.Windows.Forms.DataGridView();
             this.TargetName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Name2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -51,14 +51,14 @@ namespace ImagePlanner
             this.TargetDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.TargetDataGrid.BackgroundColor = System.Drawing.Color.MidnightBlue;
             this.TargetDataGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Navy;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Cyan;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.TargetDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Navy;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Cyan;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.TargetDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.TargetDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.TargetDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.TargetName,
@@ -70,108 +70,117 @@ namespace ImagePlanner
             this.TransitTime,
             this.SetTime,
             this.Altitude});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.MidnightBlue;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Blue;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.TargetDataGrid.DefaultCellStyle = dataGridViewCellStyle2;
-            this.TargetDataGrid.Location = new System.Drawing.Point(0, 4);
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.MidnightBlue;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.Blue;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.TargetDataGrid.DefaultCellStyle = dataGridViewCellStyle5;
+            this.TargetDataGrid.Location = new System.Drawing.Point(2, 1);
             this.TargetDataGrid.MultiSelect = false;
             this.TargetDataGrid.Name = "TargetDataGrid";
             this.TargetDataGrid.ReadOnly = true;
             this.TargetDataGrid.RowHeadersVisible = false;
             this.TargetDataGrid.RowHeadersWidth = 123;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Navy;
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Blue;
-            this.TargetDataGrid.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.Navy;
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Blue;
+            this.TargetDataGrid.RowsDefaultCellStyle = dataGridViewCellStyle6;
             this.TargetDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.TargetDataGrid.ShowEditingIcon = false;
-            this.TargetDataGrid.Size = new System.Drawing.Size(615, 690);
+            this.TargetDataGrid.Size = new System.Drawing.Size(881, 690);
             this.TargetDataGrid.TabIndex = 0;
             this.TargetDataGrid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TargetDataGrid_CellContentClick);
             // 
             // TargetName
             // 
             this.TargetName.HeaderText = "Target";
-            this.TargetName.MinimumWidth = 15;
+            this.TargetName.MinimumWidth = 150;
             this.TargetName.Name = "TargetName";
             this.TargetName.ReadOnly = true;
-            this.TargetName.Width = 63;
+            this.TargetName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.TargetName.Width = 150;
             // 
             // Name2
             // 
             this.Name2.HeaderText = "Name2";
-            this.Name2.MinimumWidth = 15;
+            this.Name2.MinimumWidth = 150;
             this.Name2.Name = "Name2";
             this.Name2.ReadOnly = true;
-            this.Name2.Width = 66;
+            this.Name2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Name2.Width = 150;
             // 
             // Species
             // 
             this.Species.HeaderText = "Species";
-            this.Species.MinimumWidth = 15;
+            this.Species.MinimumWidth = 40;
             this.Species.Name = "Species";
             this.Species.ReadOnly = true;
+            this.Species.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.Species.Width = 70;
             // 
             // MajorAxis
             // 
             this.MajorAxis.HeaderText = "Major Axis";
-            this.MajorAxis.MinimumWidth = 15;
+            this.MajorAxis.MinimumWidth = 80;
             this.MajorAxis.Name = "MajorAxis";
             this.MajorAxis.ReadOnly = true;
+            this.MajorAxis.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.MajorAxis.Width = 80;
             // 
             // MinorAxis
             // 
             this.MinorAxis.HeaderText = "MinorAxis";
-            this.MinorAxis.MinimumWidth = 15;
+            this.MinorAxis.MinimumWidth = 80;
             this.MinorAxis.Name = "MinorAxis";
             this.MinorAxis.ReadOnly = true;
-            this.MinorAxis.Width = 77;
+            this.MinorAxis.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.MinorAxis.Width = 80;
             // 
             // RiseTime
             // 
             this.RiseTime.HeaderText = "Rise";
-            this.RiseTime.MinimumWidth = 15;
+            this.RiseTime.MinimumWidth = 84;
             this.RiseTime.Name = "RiseTime";
             this.RiseTime.ReadOnly = true;
-            this.RiseTime.Width = 53;
+            this.RiseTime.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.RiseTime.Width = 84;
             // 
             // TransitTime
             // 
             this.TransitTime.HeaderText = "Transit";
-            this.TransitTime.MinimumWidth = 15;
+            this.TransitTime.MinimumWidth = 64;
             this.TransitTime.Name = "TransitTime";
             this.TransitTime.ReadOnly = true;
+            this.TransitTime.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.TransitTime.Width = 64;
             // 
             // SetTime
             // 
             this.SetTime.HeaderText = "Set";
-            this.SetTime.MinimumWidth = 15;
+            this.SetTime.MinimumWidth = 64;
             this.SetTime.Name = "SetTime";
             this.SetTime.ReadOnly = true;
-            this.SetTime.Width = 48;
+            this.SetTime.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.SetTime.Width = 64;
             // 
             // Altitude
             // 
             this.Altitude.HeaderText = "Alt (now)";
-            this.Altitude.MinimumWidth = 15;
+            this.Altitude.MinimumWidth = 75;
             this.Altitude.Name = "Altitude";
             this.Altitude.ReadOnly = true;
-            this.Altitude.Width = 73;
+            this.Altitude.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Altitude.Width = 75;
             // 
             // FormTargetList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(633, 691);
+            this.ClientSize = new System.Drawing.Size(891, 700);
             this.Controls.Add(this.TargetDataGrid);
             this.Name = "FormTargetList";
             this.Text = "Current Target Listing";
@@ -183,14 +192,14 @@ namespace ImagePlanner
         #endregion
 
         private System.Windows.Forms.DataGridView TargetDataGrid;
-        private System.Windows.Forms.DataGridViewTextBoxColumn TargetName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Name2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Species;
-        private System.Windows.Forms.DataGridViewTextBoxColumn MajorAxis;
-        private System.Windows.Forms.DataGridViewTextBoxColumn MinorAxis;
-        private System.Windows.Forms.DataGridViewTextBoxColumn RiseTime;
-        private System.Windows.Forms.DataGridViewTextBoxColumn TransitTime;
-        private System.Windows.Forms.DataGridViewTextBoxColumn SetTime;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Altitude;
+        private DataGridViewTextBoxColumn TargetName;
+        private DataGridViewTextBoxColumn Name2;
+        private DataGridViewTextBoxColumn Species;
+        private DataGridViewTextBoxColumn MajorAxis;
+        private DataGridViewTextBoxColumn MinorAxis;
+        private DataGridViewTextBoxColumn RiseTime;
+        private DataGridViewTextBoxColumn TransitTime;
+        private DataGridViewTextBoxColumn SetTime;
+        private DataGridViewTextBoxColumn Altitude;
     }
 }
